3de68d4bd893b8fd326d191988824bcd946e4760
JJ208 <46949965+JJ208@users.noreply.github.com>
2022-08-03 03:36:07 -0700

    8/3-1

---
+++ app.js
@@ -0,0 +1,34 @@
+/*sticky nav*/
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('nav');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
+
+
+
+/*toggle button*/
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+
+
+
+
+
+
+
+
+/*slide*/
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 1,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    
+});
